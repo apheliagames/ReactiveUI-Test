d9f63f40a9f08a1d6b02bffbaed4af7aed62df85
PlainBook\Robert <robertklausch@web.de>
2018-06-11 17:05:13 +0200

    no message

---
+++ Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    Contexts contexts;
+    GameSystems gameSystems;
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        contexts = Contexts.sharedInstance;
+        gameSystems = new GameSystems(contexts);
+        gameSystems.Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameSystems.Execute();
+		
+	}
+    public void ButtonPressed()
+    {
+        contexts.input.CreateEntity().isButton = true;        
+    }   
+}
---
+++ Assets/HealthLabelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthLabelController : MonoBehaviour, IHealthListener {
+
+    [SerializeField]
+    Text label;
+    // Use this for initialization
+    void Start () {
+
+        Contexts.sharedInstance.game.CreateEntity().AddHealthListener(this);
+    }
+
+    public void OnHealth(GameEntity entity, float value)
+    {
+        label.text = "Health " + value;
+        Debug.Log("OnScore: " + value);
+    }
+}
